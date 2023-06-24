9b1253534dd39b5abd25d546e39b48e7eded8c13
Belarmino Simão Júnior <belarminosimaojunior@gmail.com>
2023-06-24 09:53:03 +0200

    Euler's Method ADDED

---
+++ Euler's Method.py
@@ -0,0 +1,26 @@
+from math import *
+
+FUNCTION = lambda x, y : -x*y
+x0 = 0
+y0 = 1
+xn = 0.5
+h = 0.1
+
+def e(f, x0, y0, xn, h):
+    k = 0
+    print('k | xk | yk')
+
+    xk = x0
+    yk = y0
+    
+    while True:
+        print('{:.5f} | {:.5f} | {:.5f}'.format(k, xk, yk))
+
+        xk = xk + h
+        yk = yk + h * (f(xk, yk))
+        k += 1
+
+        if xk > xn:
+            break
+
+e(FUNCTION, x0, y0, xn, h)
